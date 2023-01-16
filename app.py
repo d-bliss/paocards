bc92a03d535acfe3c393f9f6b842a030d48ff1ca
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-16 23:12:19 +0000

    Mon, Jan 16, 2023, 11:12 PM +00:00

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///xfinal.db")
 
 
 @app.after_request
