201c2b66320629456f4bb5414f6fe35284b7d952
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-16 23:38:34 +0000

    Mon, Jan 16, 2023, 11:38 PM +00:00

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def login():
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["user_id"]
 
         # Redirect user to home page
         return redirect("/")
