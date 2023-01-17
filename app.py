63242e1668456912dd6732ad087dccba191eded4
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-17 21:57:38 +0000

    Tue, Jan 17, 2023, 9:57 PM +00:00

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def index():
 def create():
     if request.method == "POST":
         #creat and store a list of dictionaries from the standard_cards table for 1-52
-        temp_deck = db.execute(SELECT std_card_id FROM standard_cards)
+        temp_deck = db.execute("SELECT std_card_id FROM standard_cards")
         for card in temp_deck:
             user_id = session["user_id"]
             std_card_id = 1
