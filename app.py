69a596059fe370066b79c4ac0b4e95bed541fc25
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-17 04:11:58 +0000

    Tue, Jan 17, 2023, 4:11 AM +00:00

--- app.py
+++ app.py
@@ -50,22 +50,22 @@ def index():
     return render_template("index.html")
 
 
+
 @app.route("/create", methods=["GET", "POST"])
 @login_required
 def create():
     if request.method == "POST":
+        cards = db.execute("SELECT * FROM cards")
         for card in cards:
-            standard_card_id = card["id"]
-            person = request.form.get(card["id"] + "-person") or ""
-            action = request.form.get(card["id"] + "-action") or ""
-            object = request.form.get(card["id"] + "-object") or ""
-            user_id = session["user_id"]
-            db.execute("INSERT INTO custom_cards (standard_card_id, user_id, person, action, object) VALUES (?,?,?,?,?)", standard_card_id, user_id, person, action, object)
+            person = request.form.get(f"{card['id']}-person", "")
+            action = request.form.get(f"{card['id']}-action", "")
+            object = request.form.get(f"{card['id']}-object", "")
+            db.execute("INSERT INTO custom_cards (standard_card_id, user_id, person, action, object) VALUES (?,?,?,?,?)", card["id"], session["user_id"], person, action, object)
         return redirect("/savedcards")
-
     else:
-        cards = db.execute("SELECT * FROM standard_cards")
+        cards = db.execute("SELECT * FROM cards")
         return render_template("create.html", cards=cards)
+
 
 
 
