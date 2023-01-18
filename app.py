3120c3807e85211ae09af57c47823b66431bb35f
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 02:15:05 +0000

    Wed, Jan 18, 2023, 2:15 AM +00:00

--- app.py
+++ app.py
@@ -62,13 +62,19 @@ def create():
 
         #creat and store a list your custom cards
         '''temp_deck = db.execute("SELECT standard_cards.std_card_id, custom_cards.cust_card_id, custom_cards.user_id, custom_cards.person, custom_cards.action, custom_cards.obj FROM standard_cards JOIN custom_cards ON standard_cards.std_card_id = custom_cards.std_card_id AND custom_cards.user_id = :user_id", user_id=user_id)'''
-        '''temp_deck = list(range(52))'''
-        for card in cards:
-            person = request.form.get(f"person_{card.std_card_id}") or ""
+        '''temp_deck = list(range(52))
+        for card in cards:'''
+
+        person = request.form.get(f"person_1") or ""
+        action = request.form.get(f"action_1") or ""
+        obj = request.form.get(f"obj_1") or ""
+        db.execute("INSERT INTO custom_cards (user_id, std_card_id, person, action, obj) VALUES (?,?,?,?,?)", (user_id, 1, person, action, obj));
+
+           ''' person = request.form.get(f"person_{card.std_card_id}") or ""
             action = request.form.get(f"action_{card.std_card_id}") or ""
             obj = request.form.get(f"obj_{card.std_card_id}") or ""
-            db.execute("INSERT INTO custom_cards (user_id, std_card_id, person, action, obj) VALUES (?,?,?,?,?)", (user_id, card.std_card_id, person, action, obj));
-            return redirect("/savedcards")
+            db.execute("INSERT INTO custom_cards (user_id, std_card_id, person, action, obj) VALUES (?,?,?,?,?)", (user_id, card.std_card_id, person, action, obj));'''
+        return redirect("/savedcards")
 
 
     else:
