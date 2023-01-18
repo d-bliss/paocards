65de5a1300bdf91dfb41b316b6404909a27fdf56
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 16:26:50 +0000

    Wed, Jan 18, 2023, 4:26 PM +00:00

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def create():
         user_id = session["user_id"]
         cards = db.execute("SELECT * FROM standard_cards")
         persons = db.execute("SELECT person FROM custom_cards WHERE user_id = ?", user_id)
-        return render_template("create.html", cards=cards)
+        return render_template("create.html", cards=cards, persons=persons)
 
 
 
