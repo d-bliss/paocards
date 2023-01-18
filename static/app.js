e172a438cd30a0b2eaff6e859627f863327db537
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 22:16:44 +0000

    Wed, Jan 18, 2023, 10:16 PM +00:00

--- static/app.js
+++ static/app.js
@@ -1,5 +1,9 @@
 const cards = cardsData;
-// Javascript that 
+Copy code
+// This code is used to populate the form inputs with data passed in the "cardsData" variable.
+// It uses the .forEach method to iterate through each card in the "cards" array and sets the value
+// of the input fields with the corresponding data from the card object (card.person, card.action, card.obj)
+// The input fields are selected by their name attribute, which includes the std_card_id of the card.
 cards.forEach((card) => {
     const personInput = document.querySelector(
         `input[name='person_${card.std_card_id}']`
