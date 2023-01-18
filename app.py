8ac44c80260648e24a7e6da967b3b233c5068e10
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 04:03:34 +0000

    Wed, Jan 18, 2023, 4:03 AM +00:00

--- app.py
+++ app.py
@@ -54,13 +54,13 @@ def index():
 @app.route("/create", methods=["GET", "POST"])
 @login_required
 def create():
-    print(request.form)
     if request.method == "POST":
         user_id = session["user_id"]
         deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
 
         #check if user has already user_id in custom_cards table
         results = db.execute("SELECT CASE WHEN EXISTS (SELECT user_id FROM custom_cards WHERE user_id = ?) THEN 'TRUE' ELSE 'FALSE' END;",(user_id,))
+        print(results)
         if results == 'TRUE':
             for i in deck:
                 person = request.form.get(f"person_{i}") or ""
