8db7ecd673eb9852353d74a24746ab882edfc5b9
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 03:50:38 +0000

    Wed, Jan 18, 2023, 3:50 AM +00:00

--- app.py
+++ app.py
@@ -66,7 +66,9 @@ def create():
                 person = request.form.get(f"person_{i}") or ""
                 action = request.form.get(f"action_{i}") or ""
                 obj = request.form.get(f"obj_{i}") or ""
-                db.execute("REPLACE INTO custom_cards (user_id, std_card_id, person, action, obj) VALUES (?,?,?,?,?)", user_id, i, person, action, obj);
+                '''db.execute("UPDATE custom_cards SET (user_id, std_card_id, person, action, obj) VALUES (?,?,?,?,?)", user_id, i, person, action, obj);'''
+                db.execute("UPDATE custom_cards SET std_card_id = ?, person = ?, action = ?, obj = ? WHERE user_id = ?", i, person, action, obj, user_id);
+
         else:
             for i in deck:
                 person = request.form.get(f"person_{i}") or ""
