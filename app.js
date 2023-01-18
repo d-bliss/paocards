14fda52b8782263d1e2410dabcd0722ab8f4eede
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 17:39:55 +0000

    Wed, Jan 18, 2023, 5:39 PM +00:00

---
+++ app.js
@@ -0,0 +1,12 @@
+const cards = <%= JSON.stringify(cards) %>;
+
+cards.forEach(card => {
+    const personInput = document.querySelector(`input[name='person_${card.std_card_id}']`);
+    personInput.value = card.person;
+
+    const actionInput = document.querySelector(`input[name='action_${card.std_card_id}']`);
+    actionInput.value = card.action;
+
+    const objInput = document.querySelector(`input[name='obj_${card.std_card_id}']`);
+    objInput.value = card.obj;
+});
