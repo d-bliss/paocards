e581a64cc14d5161ce5f64ef1bbcdd7a038554bf
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 02:43:20 +0000

    Wed, Jan 18, 2023, 2:43 AM +00:00

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def create():
         for card in cards:'''
         temp_deck = list(range(52))
         for i in temp_deck:
-            person = request.form.get(f"person_" + str(i)) or ""
+            person = request.form.get(f"person_{i}") or ""
             action = request.form.get(f"action_1") or ""
             obj = request.form.get(f"obj_1") or ""
             db.execute("INSERT INTO custom_cards (user_id, std_card_id, person, action, obj) VALUES (?,?,?,?,?)", user_id, i, person, action, obj);
