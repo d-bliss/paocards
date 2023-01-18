baa9d99fee7c74694d3b90d929bb5eab191642fa
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-18 04:34:27 +0000

    Wed, Jan 18, 2023, 4:34 AM +00:00

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ def create():
         deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
 
         #check if user has already user_id in custom_cards table
-        result = db.execute("SELECT EXISTS (SELECT 1 FROM custom_cards WHERE user_id = ?)", (user_id,)).fetchone()[0]
+        result = db.execute("SELECT EXISTS (SELECT 1 FROM custom_cards WHERE user_id = ?)", (user_id,))[0]['EXISTS (SELECT 1 FROM custom_cards WHERE user_id = ?)']
+
         if results == 1:
             for i in deck:
                 person = request.form.get(f"person_{i}") or ""
