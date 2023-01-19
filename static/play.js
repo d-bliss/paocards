db018b62d166727cde4aa9e5a072b38d97632cdd
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-19 04:54:28 +0000

    Thu, Jan 19, 2023, 4:54 AM +00:00

--- static/play.js
+++ static/play.js
@@ -3,18 +3,25 @@ let currentCardIndex = {{ current_card_index }};
 let currentCard = cards[currentCardIndex];
 let isFlipped = {{ flip }};
 
+// Select the element with id "card-name"
 const cardName = document.querySelector("#card-name");
+// Set the text of the element to the current card's rank and suit
 cardName.innerText = `${currentCard.rank} of ${currentCard.suit}`;
+
+// Select the element with id "card-attributes"
 const cardAttributes = document.querySelector("#card-attributes");
 
-
+// Select the element with id "flip-button"
 const flipButton = document.querySelector("#flip-button");
+// Add an event listener to the button that toggles the display of the card attributes
 flipButton.addEventListener("click", () => {
     isFlipped = !isFlipped;
     cardAttributes.style.display = isFlipped ? "block" : "none";
 });
 
+// Select the element with id "next-button"
 const nextButton = document.querySelector("#next-button");
+// Add an event listener to the button that displays the next card in the cards array
 nextButton.addEventListener("click", () => {
     currentCardIndex = (currentCardIndex + 1) % cards.length;
     currentCard = cards[currentCardIndex];
