d3b95f4bfc48ee29709fa948d1a04c85d2c59723
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-20 22:29:40 +0000

    Fri, Jan 20, 2023, 10:29 PM +00:00

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:/workspaces/xfinal/xfinal.db")
+db = SQL("sqlite:///xfinal.db")
 
 
 @app.after_request
