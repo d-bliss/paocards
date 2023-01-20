6420fbe520405803d927c22e27ab8e6a02b002ab
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-20 02:31:30 +0000

    Fri, Jan 20, 2023, 2:31 AM +00:00

--- app.py
+++ app.py
@@ -87,26 +87,7 @@ def create():
         cards = db.execute("SELECT standard_cards.*, custom_cards.person, custom_cards.action, custom_cards.obj FROM standard_cards LEFT JOIN custom_cards ON standard_cards.std_card_id=custom_cards.std_card_id and custom_cards.user_id=?", user_id)
         return render_template("create.html", cards=cards)
 
-@app.route("/play", methods=["GET", "POST"])
-def play():
-    if request.method == "GET":
-        # get the user_id from the session
-        user_id = session["user_id"]
-        # select the custom cards of the user from the db
-        cards = db.execute("SELECT custom_cards.*, standard_cards.suit, standard_cards.rank FROM custom_cards JOIN standard_cards ON custom_cards.std_card_id = standard_cards.std_card_id WHERE custom_cards.user_id = ? ORDER BY custom_cards.std_card_id", user_id)
-        current_card_index = 0
-        current_card = cards[current_card_index]
-        return render_template("play.html", current_card=current_card, current_card_index=current_card_index)
 
-    elif request.method == "POST":
-        if "Flip" in request.form:
-            # Handle logic for flipping the card to show custom attributes
-            flip = not flip
-        elif "Next" in request.form:
-            # Handle logic for displaying the next card
-            current_card_index = (current_card_index + 1) % len(cards)
-            current_card = cards[current_card_index]
-        return render_template("play.html", cards=cards, current_card=current_card, current_card_index=current_card_index, flip=flip, card_images=card_images)
 
 
 @app.route("/savedcards")
