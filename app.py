30ba31b7ab130564907ea56a27314261ef87cdd0
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-20 02:57:12 +0000

    Fri, Jan 20, 2023, 2:57 AM +00:00

--- app.py
+++ app.py
@@ -93,6 +93,7 @@ def play():
     current_card = None
     flip = False
     card_images = ["AC.png", "AD.png", "AH.png", "AS.png", "2C.png", "2D.png", "2H.png", "2S.png", "3C.png", "3D.png", "3H.png", "3S.png", "4C.png", "4D.png", "4H.png", "4S.png", "5C.png", "5D.png", "5H.png", "5S.png", "6C.png", "6D.png", "6H.png", "6S.png", "7C.png", "7D.png", "7H.png", "7S.png", "8C.png", "8D.png", "8H.png", "8S.png", "9C.png", "9D.png", "9H.png", "9S.png", "10C.png", "10D.png", "10H.png", "10S.png", "JC.png", "JD.png", "JH.png", "JS.png", "QC.png", "QD.png", "QH.png", "QS.png", "KC.png", "KD.png", "KH.png", "KS.png"]
+    cards = []
     if request.method == "GET":
         # get the user_id from the session
         user_id = session["user_id"]
