5e462ccadb9bd0b959a91ddffc2c1787974ed8f8
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-20 17:45:46 +0000

    Fri, Jan 20, 2023, 5:45 PM +00:00

--- app.py
+++ app.py
@@ -101,19 +101,18 @@ def play():
             # retrieve custom card attributes from custom_cards table
             current_card = db.execute("SELECT person, action, obj FROM custom_cards WHERE user_id = ? AND std_card_id = ?", user_id, card_index)
         elif request.form.get("Next"):
-            if card_index + 1 < len(card_images)::
-            card_index += 1
-            session["card_index"] = card_index
-            current_card = db.execute("SELECT person, action, obj FROM custom_cards WHERE user_id = ? AND std_card_id = ?", user_id, card_index)
-        else:
-            flash("No more cards to show.")
-            card_index = 0
-            session["card_index"] = card_index
-            current_card = db.execute("SELECT person, action, obj FROM custom_cards WHERE user_id = ? AND std_card_id = ?", user_id, card_index)
-        if card_index + 1 < len(card_images):
-            return render_template("play.html", card_index=card_index, card_images=card_images, current_card=current_card)
-        else:
-            return redirect("/index")
+            if card_index + 1 < len(card_images):
+                card_index += 1
+                session["card_index"] = card_index
+                current_card = db.execute("SELECT person, action, obj FROM custom_cards WHERE user_id = ? AND std_card_id = ?", user_id, card_index)
+            else:
+                flash("No more cards to show.")
+                card_index = 0
+                session["card_index"] = card_index
+                current_card = db.execute("SELECT person, action, obj FROM custom_cards WHERE user_id = ? AND std_card_id = ?", user_id, card_index)
+    return render_template("play.html", card_index=card_index, card_images=card_images, current_card=current_card)
+else:
+    return redirect("/index")
 
 
 
