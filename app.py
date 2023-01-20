2c0ffc8c0af66be7cf276e4364bec4b096590994
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-20 02:43:27 +0000

    Fri, Jan 20, 2023, 2:43 AM +00:00

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def play():
         cards = db.execute("SELECT custom_cards.*, standard_cards.suit, standard_cards.rank FROM custom_cards JOIN standard_cards ON custom_cards.std_card_id = standard_cards.std_card_id WHERE custom_cards.user_id = ? ORDER BY custom_cards.std_card_id", user_id)
         current_card = cards[current_card_index]
         flip = False
-        return render_template("play.html", current_card=current_card, current_card_index=current_card_index, flip=flip, card_images=card_images)
+        return render_template("play.html", current_card=current_card, current_card_index=current_card_index, flip=flip)
 
     elif request.method == "POST":
         if "Flip" in request.form:
