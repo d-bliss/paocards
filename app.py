d6cbc45813de6d260b98f75160ba9564c734adcf
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-21 03:15:59 +0000

    Sat, Jan 21, 2023, 3:15 AM +00:00

--- app.py
+++ app.py
@@ -88,9 +88,9 @@ def create():
         return render_template("create.html", cards=cards)
 
 
-@app.route("/play/<int: card_index>'", methods=["GET", "POST"])
+@app.route("/play", methods=["GET", "POST"])
 @login_required
-def play(card_index):
+def play():
     card_index = 0
     user_id = session["user_id"]
     card_images = ["AC.png", "2C.png", "3C.png", "4C.png", "5C.png", "6C.png", "7C.png", "8C.png", "9C.png", "10C.png", "JC.png", "QC.png", "KC.png", "AD.png", "2D.png", "3D.png", "4D.png", "5D.png", "6D.png", "7D.png", "8D.png", "9D.png", "10D.png", "JD.png", "QD.png", "KD.png", "AH.png", "2H.png", "3H.png", "4H.png", "5H.png", "6H.png", "7H.png", "8H.png", "9H.png", "10H.png", "JH.png", "QH.png", "KH.png", "AS.png", "2S.png", "3S.png", "4S.png", "5S.png", "6S.png", "7S.png", "8S.png", "9S.png", "10S.png", "JS.png", "QS.png", "KS.png"]
