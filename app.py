68c0be0f4cb3513b52de311f47e1e58919254a40
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-21 03:52:42 +0000

    Sat, Jan 21, 2023, 3:52 AM +00:00

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def create():
 def play(card_index):
     if 'card_index' not in session:
         session['card_index'] = 0
-    card_index = 0
+    #card_index = 0
     user_id = session["user_id"]
     card_images = ["AC.png", "2C.png", "3C.png", "4C.png", "5C.png", "6C.png", "7C.png", "8C.png", "9C.png", "10C.png", "JC.png", "QC.png", "KC.png", "AD.png", "2D.png", "3D.png", "4D.png", "5D.png", "6D.png", "7D.png", "8D.png", "9D.png", "10D.png", "JD.png", "QD.png", "KD.png", "AH.png", "2H.png", "3H.png", "4H.png", "5H.png", "6H.png", "7H.png", "8H.png", "9H.png", "10H.png", "JH.png", "QH.png", "KH.png", "AS.png", "2S.png", "3S.png", "4S.png", "5S.png", "6S.png", "7S.png", "8S.png", "9S.png", "10S.png", "JS.png", "QS.png", "KS.png"]
     #current_card = card_index
