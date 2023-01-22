9f3fcba4d8339c26c83bb70d0c4f90eb3dae2901
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-22 00:49:10 +0000

    Sun, Jan 22, 2023, 12:49 AM +00:00

--- static/app.js
+++ static/app.js
@@ -1,55 +1,62 @@
-const cards = JSON.parse('{{ cards|tojson|safe }}');
-let currentCardIndex = {{ current_card_index }};
-let currentCard = cards[currentCardIndex];
-let isFlipped = {{ flip }};
+const cards = cardsData;
+// This code is used to populate the form inputs with data passed in the "cardsData" variable.
+// It uses the .forEach method to iterate through each card in the "cards" array and sets the value
+// of the input fields with the corresponding data from the card object (card.person, card.action, card.obj)
+// The input fields are selected by their name attribute, which includes the std_card_id of the card.
+cards.forEach((card) => {
+    const personInput = document.querySelector(
+        `input[name='person_${card.std_card_id}']`
+    );
+    personInput.value = card.person;
+    personInput.classList.add("bold-text");
 
-// Select the container element where the cards will be displayed
-const cardsContainer = document.querySelector('.cards-container');
 
-// Loop through the cardsData array
-for (let i = 0; i < cardsData.length; i++) {
-    // Create a new div element for each card
-    let cardDiv = document.createElement('div');
-    // Add a class to the div element for styling
-    cardDiv.classList.add('card');
-    // Set the card's rank and suit as the div's innerHTML
-    cardDiv.innerHTML = `${cardsData[i].rank} of ${cardsData[i].suit}`;
-    // Add the person, action and object to the card
-    cardDiv.innerHTML += `<br> Person: ${cardsData[i].person} <br> Action: ${cardsData[i].action} <br> Object: ${cardsData[i].obj}`;
-    // Append the card div to the container element
-    cardsContainer.appendChild(cardDiv);
-}
+    const actionInput = document.querySelector(
+        `input[name='action_${card.std_card_id}']`
+    );
+    actionInput.value = card.action;
+    actionInput.classList.add("bold-text");
 
-// Select the element with id "card-name"
-const cardName = document.querySelector("#card-name");
-// Set the text of the element to the current card's rank and suit
-cardName.innerText = `${currentCard.rank} of ${currentCard.suit}`;
 
-// Select the element with id "card-attributes"
-const cardAttributes = document.querySelector("#card-attributes");
+    const objInput = document.querySelector(
+        `input[name='obj_${card.std_card_id}']`
+    );
+    objInput.value = card.obj;
+    objInput.classList.add("bold-text");
 
-// Select the element with id "flip-button"
-const flipButton = document.querySelector("#flip-button");
-
-// Select the image element
-const image = document.querySelector("#card-image");
-
-// Add an event listener to the button that toggles the display of the card attributes
-flipButton.addEventListener("click", () => {
-    isFlipped = !isFlipped;
-    cardAttributes.style.display = isFlipped ? "block" : "none";
-    image.src = isFlipped ? "{{ url_for('static', filename='playing-cards/custom/' + current_card.img_path) }}" : "{{ url_for('static', filename='playing-cards/' + cards[currentCardIndex].img_path) }}";
 });
 
-// Select the element with id "next-button"
-const nextButton = document.querySelector("#next-button");
-// Add an event listener to the button that displays the next card in the cards array
-nextButton.addEventListener("click", () => {
-    currentCardIndex = (currentCardIndex + 1) % cards.length;
-    currentCard = cards[currentCardIndex];
+
+<script>
+    // parse the cards data passed from the backend
+    const cards = JSON.parse('{{ cards|tojson|safe }}');
+    let currentCardIndex = {{ current_card_index }};
+    let currentCard = cards[currentCardIndex];
+    let isFlipped = {{ flip }};
+
+    //update the card name and attributes displayed on the page
+    const cardName = document.querySelector("#card-name");
     cardName.innerText = `${currentCard.rank} of ${currentCard.suit}`;
+    const cardAttributes = document.querySelector("#card-attributes");
     cardAttributes.innerHTML = `Person: ${currentCard.person}<br>
                                 Action: ${currentCard.action}<br>
                                 Object: ${currentCard.obj}`;
-    image.src = "{{ url_for('static', filename='playing-cards/' + cards[currentCardIndex].img_path) }}";
-});
+
+    // toggle the visibility of the card attributes when the flip button is clicked
+    const flipButton = document.querySelector("#flip-button");
+    flipButton.addEventListener("click", () => {
+        isFlipped = !isFlipped;
+        cardAttributes.style.display = isFlipped ? "block" : "none";
+    });
+
+    // increment the current_card_index and update the current card displayed when the next button is clicked
+    const nextButton = document.querySelector("#next-button");
+    nextButton.addEventListener("click", () => {
+        currentCardIndex = (currentCardIndex + 1) % cards.length;
+        currentCard = cards[currentCardIndex];
+        cardName.innerText = `${currentCard.rank} of ${currentCard.suit}`;
+        cardAttributes.innerHTML = `Person: ${currentCard.person}<br>
+                                    Action: ${currentCard.action}<br>
+                                    Object: ${currentCard.obj}`;
+    });
+</script>
