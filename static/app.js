e686f3fd43c534c81054adfdc8759c4f3eebc6b4
Bliss <113086691+d-bliss@users.noreply.github.com>
2023-01-22 06:18:51 +0000

    Sun, Jan 22, 2023, 6:18 AM +00:00

--- static/app.js
+++ static/app.js
@@ -30,4 +30,36 @@ cards.forEach((card) => {
     // parse the cards data passed from the backend
     const cards = JSON.parse('{{ cards|tojson|safe }}');
 </script>
-*/
+*/<script>
+    // parse the cards data passed from the backend
+    const cards = JSON.parse('{{ cards|tojson|safe }}');
+    let currentCardIndex = {{ current_card_index }};
+    let currentCard = cards[currentCardIndex];
+    let isFlipped = {{ flip }};
+
+    //update the card name and attributes displayed on the page
+    const cardName = document.querySelector("#card-name");
+    cardName.innerText = `${currentCard.rank} of ${currentCard.suit}`;
+    const cardAttributes = document.querySelector("#card-attributes");
+    cardAttributes.innerHTML = `Person: ${currentCard.person}<br>
+                                Action: ${currentCard.action}<br>
+                                Object: ${currentCard.obj}`;
+
+    // toggle the visibility of the card attributes when the flip button is clicked
+    const flipButton = document.querySelector("#flip-button");
+    flipButton.addEventListener("click", () => {
+        isFlipped = !isFlipped;
+        cardAttributes.style.display = isFlipped ? "block" : "none";
+    });
+
+    // increment the current_card_index and update the current card displayed when the next button is clicked
+    const nextButton = document.querySelector("#next-button");
+    nextButton.addEventListener("click", () => {
+        currentCardIndex = (currentCardIndex + 1) % cards.length;
+        currentCard = cards[currentCardIndex];
+        cardName.innerText = `${currentCard.rank} of ${currentCard.suit}`;
+        cardAttributes.innerHTML = `Person: ${currentCard.person}<br>
+                                    Action: ${currentCard.action}<br>
+                                    Object: ${currentCard.obj}`;
+    });
+</script>
